c67ed08e3f93372e6d95ecf2af4ff4331e46cc56
Edwin Chalumeau <edwinchalumeau@outlook.com>
2017-12-21 16:33:48 -0500

    Created homework.js

---
+++ Homework1.js
@@ -0,0 +1,9 @@
+var fname = prompt("What is your name?");
+confirm("Is this correct? Your name is " + fname);
+var gender = prompt("Are you a boy or girl?");
+confirm("You are a " + gender);
+var age = prompt("How old are you?");
+if(age<13){
+  alert("you are too young to have a pokemon");
+
+}
